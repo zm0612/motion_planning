01b4f3f6300660a3f94c109daa35ac65c6307b92
Zhang Zhimeng <849742405@qq.com>
2021-09-08 00:26:03 +0800

    1.Add some codes of lecture 5

--- lecture_5_minimum_snap_trajectoty_generation/src/waypoint_trajectory_generator/src/trajectory_generator_waypoint.cpp
+++ lecture_5_minimum_snap_trajectoty_generation/src/waypoint_trajectory_generator/src/trajectory_generator_waypoint.cpp
@@ -46,14 +46,15 @@ Eigen::MatrixXd TrajectoryGeneratorWaypoint::PolyQPGeneration(
         const Eigen::VectorXd &Time)          // 每一段轨迹的时间
 { //给定起始点和终点的速度加速度，更高阶的导数设置为0
 
-    int p_order = 2 * d_order - 1;              //多项式的最高次数 p^(p_order)t^(p_order) + ...
-    int p_num1d = p_order + 1;                  //每一段轨迹的变量个数，对于五阶多项式为：p5, p4, ... p0
+    const int p_order = 2 * d_order - 1;              //多项式的最高次数 p^(p_order)t^(p_order) + ...
+    const int p_num1d = p_order + 1;                  //每一段轨迹的变量个数，对于五阶多项式为：p5, p4, ... p0
 
-    int number_segments = Time.size();
+    const int number_segments = Time.size();
     //每一段都有x,y,z三个方向，每一段多项式的系数的个数有3*p_num1d
     MatrixXd PolyCoeff = MatrixXd::Zero(number_segments, 3 * p_num1d);
     //整条轨迹在ｘ,y,z方向上共多少个未知系数
-    VectorXd Px(p_num1d * number_segments), Py(p_num1d * number_segments), Pz(p_num1d * number_segments);
+    const int number_coefficients = p_num1d * number_segments;
+    VectorXd Px(number_coefficients), Py(number_coefficients), Pz(number_coefficients);
 
     /*   Produce Mapping Matrix M to the entire trajectory, M is a mapping matrix that maps polynomial coefficients to derivatives.   */
     const int M_block_rows = d_order * 2;
@@ -61,19 +62,28 @@ Eigen::MatrixXd TrajectoryGeneratorWaypoint::PolyQPGeneration(
     MatrixXd M = MatrixXd::Zero(number_segments * M_block_rows, number_segments * M_block_cols);
     for (int i = 0; i < number_segments; ++i) {
         int row = i * M_block_rows, col = i * M_block_cols;
-        MatrixXd sum_M = MatrixXd::Zero(M_block_rows, M_block_cols);
+        MatrixXd sub_M = MatrixXd::Zero(M_block_rows, M_block_cols);
 
         for (int j = 0; j < d_order; ++j) {
             for (int k = 0; k < p_num1d; ++k) {
                 if (k < j)
                     continue;
 
-                sum_M(j, k) = Factorial(k) / Factorial(k - j) * pow(0, k - j);
-                sum_M(j + d_order, k) = Factorial(k) / Factorial(k - j) * pow(Time(i), k - j);
+                sub_M(j, p_num1d - 1 - k) = Factorial(k) / Factorial(k - j) * pow(0, k - j);
+                sub_M(j + d_order, p_num1d - 1 - k) = Factorial(k) / Factorial(k - j) * pow(Time(i), k - j);
             }
         }
 
-        M.block(row, col, M_block_rows, M_block_cols) = sum_M;
+        M.block(row, col, M_block_rows, M_block_cols) = sub_M;
+    }
+
+    //构造选择矩阵C的过程非常复杂，但是只要多花点时间探索一些规律，应该是能写出来的!!
+    const int number_valid_variables = (number_segments + 1) * d_order;
+    MatrixXd C_T = MatrixXd::Zero(number_coefficients, number_valid_variables);
+    for (int i = 0; i <number_coefficients; ++i) {
+        for (int j = 0; j < number_valid_variables; ++j) {
+            
+        }
     }
 
     /*   Produce the dereivatives in X, Y and Z axis directly.  */
